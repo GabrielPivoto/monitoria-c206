a54bbd9e34af2c1bd18254195b2fae3e5e38ab4a
Gabriel Pivoto <65688445+GabrielPivoto@users.noreply.github.com>
2022-03-17 10:45:31 -0300

    Create Produto.java

---
+++ Exercicios/Aula04/Exercicio1/src/main/java/Produto.java
@@ -0,0 +1,13 @@
+public class Produto {
+    public String nome;
+    public String descricao;
+    public String fabricante;
+    public double preco;
+
+    public Produto(String nome, String descricao, String fabricante, double preco){
+        this.nome = nome;
+        this.descricao = descricao;
+        this.fabricante = fabricante;
+        this.preco = preco;
+    }
+}
